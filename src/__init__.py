5a94ad7937713acd86dc53595df3cdbe04383a38
jy392 <jy392@cornell.edu>
2020-12-06 23:28:42 -0500

    docker setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy 
 
 
 # instantiate the app
@@ -8,9 +10,24 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  
+app.config.from_object(app_settings)      
 
+# instantiate the db
+db = SQLAlchemy(app) 
 
+# model
+class User(db.Model):  
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        
 class Ping(Resource):
     def get(self):
         return {
