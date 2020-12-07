2aab0204d65ed416c0d9c3e33b6e5f59e33aef4e
jy392 <jy392@cornell.edu>
2020-12-06 23:39:38 -0500

    flask bluepritns

--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ import os
 
 class BaseConfig:
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_secret_key'  
 
 
 class DevelopmentConfig(BaseConfig):
